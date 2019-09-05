1ace2240d91babafa67000c51cf8e18351a9ae89
basseyemmanuel123 <basseyemmanuel123@gmail.com>
2019-09-05 05:56:31 +0100

    Almost Done!

--- app/src/main/java/com/example/recipeapp/RecipeActivity.java
+++ app/src/main/java/com/example/recipeapp/RecipeActivity.java
@@ -2,12 +2,61 @@ package com.example.recipeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.example.recipeapp.adapter.RecipeAdapter;
+import com.example.recipeapp.api.RecipeAPI;
+import com.example.recipeapp.api.RecipeClient;
+import com.example.recipeapp.model.RecipeModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    static List<RecipeModel> recipesList;
+    private RecyclerView recyclerView;
+    private RecipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        recyclerView =  findViewById(R.id.recyclerView);
+        recipesList = new ArrayList<>();
+        adapter = new RecipeAdapter(this, recipesList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        RecipeAPI service = RecipeClient.getClient().create(RecipeAPI.class);
+        Call<List<RecipeModel>> call = service.readJson();
+        call.enqueue(new Callback<List<RecipeModel>>() {
+            @Override
+            public void onResponse(Call<List<RecipeModel>> call, Response<List<RecipeModel>> response) {
+                recipesList = response.body();
+                adapter.setRecipes(recipesList);
+            }
+
+            @Override
+            public void onFailure(Call<List<RecipeModel>> call, Throwable t) {
+                Log.e("RecipeActivity", "Error calling API", t);
+            }
+        });
+    }
+
+    public  static RecipeModel getRecipe(int position){
+        return recipesList.get(position);
     }
 }
--- app/src/main/java/com/example/recipeapp/adapter/RecipeAdapter.java
+++ app/src/main/java/com/example/recipeapp/adapter/RecipeAdapter.java
@@ -1,4 +1,89 @@
 package com.example.recipeapp.adapter;
 
-public class RecipeAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.recipeapp.R;
+import com.example.recipeapp.model.RecipeModel;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+
+    Context mContext;
+    List<RecipeModel> recipes;
+    RecipeOnclickListener mListener;
+
+    public RecipeAdapter(Context mContext, List<RecipeModel> recipes) {
+        this.mContext = mContext;
+        this.recipes = recipes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.recipe_list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        RecipeModel recipeModel = recipes.get(i);
+        viewHolder.name.setText(recipeModel.getName());
+
+        Glide.with(mContext).load(recipeModel.getImage())
+                .error(R.drawable.ic_launcher_background)
+                .apply(RequestOptions.centerCropTransform())
+                .into(viewHolder.image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes == null ? 0 : recipes.size();
+    }
+
+    public void setRecipes(List<RecipeModel> recipes){
+        for(RecipeModel recipe:recipes){
+            addRecipe(recipe);
+        }
+    }
+
+    public void addRecipe(RecipeModel recipe){
+        recipes.add(recipe);
+        notifyItemInserted(recipes.size());
+    }
+
+    public interface  RecipeOnclickListener{
+        void onClick(int position);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView name;
+        private CircleImageView image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.recipe_name);
+            image = itemView.findViewById(R.id.recipe_image);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
